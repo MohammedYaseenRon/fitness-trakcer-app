d3966c7719389e4de1f6aabcf889e0d66e2493b2
MohammedYaseenRon <yaseenron3@gmail.com>
2024-09-23 01:27:28 +0530

    ui made and added

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,12 +1,30 @@
-
 import React from 'react';
+import Image from 'next/image';
+import { ServiceInfo } from '@/components/ServiceInfo';
+import { Footer } from '@/components/Landing/Footer';
+
 
 export default function ServicesPage() {
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-4">Our Services</h1>
-      <p className="mb-4">Here you can list and describe your fitness app services.</p>
-      {/* Add more content about your services here */}
+    <div className="h-screnn">
+      <div className="w-full px-32 py-12 ">
+        <Image
+          src="/Assets/banner (2).png"
+          alt="Fitness"
+          width={100}
+          height={100} 
+          layout="responsive" 
+          className="rounded-xl"
+        />
+      </div>
+      <div className="text-center">
+        <h1 className="text-2xl text-slate-900 font-frank">Why Choose Us?</h1>
+        <p className="text-4xl text-slate-600 font-frank mt-2">Comprhensive<span className="text-blue-600"> Services</span></p>
+      </div>
+        <div className="px-32 py-12">
+          <ServiceInfo />
+        </div>
+        <Footer />
     </div>
   );
 }
